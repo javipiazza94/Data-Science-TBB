91b47f23e3681154ae5ae6a29e97defe86ea7548
javipiazza94 <javipiazza94@gmail.com>
2023-12-20 17:02:43 +0100

    COSITAS de Web Scraping

--- SCRIPTS/Web Scraping/Buscar_Google.py
+++ SCRIPTS/Web Scraping/Buscar_Google.py
@@ -8,12 +8,17 @@ driver = webdriver.Firefox()
 # Cargar una página web de ejemplo
 driver.get('https://www.google.com')
 time.sleep(5)
-cookies = driver.find_element(By.CSS_SELECTOR, 'button#L2AGLb')
-cookies.click()
-time.sleep(5)
+
+# Aceptar cookies
+try:
+    cookies_button = driver.find_element(By.ID, 'L2AGLb')  # Utilizando ID en lugar de CSS_SELECTOR
+    driver.execute_script("arguments[0].scrollIntoView();", cookies_button)
+    cookies_button.click()
+except Exception as e:
+    print(f"Error al hacer clic en cookies: {e}")
 
 # Realizar acciones en la página (por ejemplo, buscar en Google)
-search_box = driver.find_element(By.XPATH, "//textarea[@class = 'gLFyf']")  # Buscar por css
+search_box = driver.find_element(By.XPATH, "//textarea[@class = 'gLFyf']")
 search_box.send_keys('Selenium with Python')
 search_box.submit()
 
